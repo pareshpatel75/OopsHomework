8e2ced99ed096890764b16d07725880863ad1c56
pareshpatel75 <102856171+pareshpatel75@users.noreply.github.com>
2022-05-10 21:13:17 +0100

    Delete India.java

--- India.java
+++
@@ -1,9 +0,0 @@
-package OopsHomework;
-
-public class India extends World{
-
-    public void Gujarat (){
-        System.out.println("Rich State");
-        System.out.println("peaceful State");
-    }
-}
